cf2fa24eb87c81d50f6c966bf20e9a674eeaed6a
Alan Corona <alan654ghQgmail.com>
2023-01-14 10:01:15 -0600

    Troll

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+import random
+import time
+import webbrowser
+
+while True:
+    pages = random.choice(["trollface.dk", "www.youtube.com/watch?v=2VXacYLcjGA",
+                           "media.giphy.com/media/eVy46EWyclTIA/giphy.gif"])  # Websites to open
+    visit = "https://{}".format(pages)
+    timeWait = random.randrange(1, 3)  # Delay
+    time.sleep(timeWait)
+    webbrowser.open(pages)  # Open Pages
